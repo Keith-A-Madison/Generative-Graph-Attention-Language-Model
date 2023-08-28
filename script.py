16e9f5900e5ef8dae14269134d6e16c2a4655a0a
Keith Madison <96896521+Keith-A-Madison@users.noreply.github.com>
2023-08-28 16:58:16 -0400

    Update script.py
    Begun working on attention mechanism, visualization for GCN layers.

--- script.py
+++ script.py
@@ -6,6 +6,9 @@ from spektral.utils import normalized_adjacency
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from data import get_training_data
 
+import networkx as nx
+import matplotlib.pyplot as plt
+
 # Need to change all the 9's as soon as the vocab size grows :(
 corpus = get_training_data("/home/kmadison/gpt/GenAtten/training/") # ["The quick brown fox jumps", "over the lazy dog", "and goes to the store", "to buy some tacos"] #, "and goes to the store", "to buy some tacos"]
 tokens = [sentence.split() for sentence in corpus]
@@ -34,8 +37,16 @@ adjacency_matrix = normalized_adjacency(similarity_matrix)
 
 input_layer = Input(shape=(None,))
 embedding = Embedding(input_dim=vocab_size, output_dim=128)(input_layer)
-gcn_layer = GCNConv(128)([embedding, adjacency_matrix])
-output_layer = Dense(vocab_size, activation='softmax')(gcn_layer)
+
+n_heads = 2
+gcn_layers = []
+
+for _ in range(n_heads):
+    gcn_layer = GCNConv(128)([embedding, adjacency_matrix])
+    gcn_layers.append(gcn_layer)
+
+concatenated_gcn = tf.keras.layers.concatenate(gcn_layers, axis=-1)
+output_layer = Dense(vocab_size, activation='softmax')(concatenated_gcn)
 
 model = tf.keras.Model(inputs=input_layer, outputs=output_layer)
 model.compile(optimizer='adam', loss='categorical_crossentropy')
@@ -61,7 +72,7 @@ print(train_data_padded)
 train_data_padded = np.array(train_data_padded)
 train_labels = np.array(train_labels)
 
-num_epochs = 100
+num_epochs = 25
 # Training loop
 model.fit(train_data_padded, tf.keras.utils.to_categorical(train_labels, num_classes=vocab_size), epochs=num_epochs)
 
@@ -104,3 +115,56 @@ generated_text = [token for token in generated_text if token != padding_token]
 # Join the tokens into text
 generated_text = " ".join(generated_text)
 print("Generated Text:", generated_text)
+
+# ... Your existing code ...
+
+# Create a NetworkX graph from the adjacency matrix
+graph = nx.Graph()
+for i in range(len(vocab)):
+    graph.add_node(i)
+
+for i in range(len(vocab)):
+    for j in range(len(vocab)):
+        if adjacency_matrix[i, j] != 0:
+            graph.add_edge(i, j, weight=adjacency_matrix[i, j])
+
+# ... Your existing code ...
+
+# Visualize each GCN layer
+for i, gcn_layer in enumerate(gcn_layers):
+    # Create a new NetworkX graph for visualization
+    gcn_graph = nx.Graph()
+
+    # Extract the node embeddings from the GCN layer
+    node_embeddings = gcn_layer[0]
+
+    for node_idx in range(len(vocab)):
+        gcn_graph.add_node(node_idx, embedding=node_embeddings[node_idx])
+
+    for i in range(len(vocab)):
+        for j in range(len(vocab)):
+            if adjacency_matrix[i, j] != 0:
+                weight = adjacency_matrix[i, j]
+                gcn_graph.add_edge(i, j, weight=weight)
+
+    # Visualize the GCN layer
+    plt.figure(figsize=(8, 6))
+    pos = nx.spring_layout(gcn_graph, seed=42)
+
+    # Draw nodes
+    nodes = nx.draw_networkx_nodes(gcn_graph, pos, node_size=50, node_color='b', alpha=0.7)
+
+    # Draw edges with color based on weights
+    edge_weights = np.array([gcn_graph[u][v]['weight'] for u, v in gcn_graph.edges()])
+    edge_norm = plt.Normalize(vmin=edge_weights.min(), vmax=edge_weights.max())
+    for (u, v), weight in zip(gcn_graph.edges(), edge_weights):
+        nx.draw_networkx_edges(gcn_graph, pos, edgelist=[(u, v)], edge_color=plt.cm.Blues(edge_norm(weight)), width=2)
+
+    # Add labels to nodes corresponding to words
+    labels = {node_idx: idx2word[node_idx] for node_idx in gcn_graph.nodes()}
+    nx.draw_networkx_labels(gcn_graph, pos, labels, font_size=8)
+
+    plt.title(f"GCN Layer {i + 1} Visualization")
+    plt.colorbar(plt.cm.ScalarMappable(norm=edge_norm, cmap=plt.cm.Blues))
+    plt.savefig(f"gcn_layer_{i + 1}_visualization.png")
+    plt.show()              
